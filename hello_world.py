49ba333288b3228e2f6f9dce00a4b0bb5f9e068d
Sidney <sidney.kang@ocsbstudent.ca>
2017-09-13 16:06:18 -0400

    Edited version: added comment at beggining

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+#Created by : Sidney Kang
+#Created on : 8 Sept. 2017
+#Created for : ICS3UR
+# Daily Assignment - Unit0-01
+# This program is the hello, world program
+
+print("Hello, World!")
+print("Sidney")
+print("Ottawa")
+print("Ontario")
